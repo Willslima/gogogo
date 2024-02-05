1a2ebd18b547ae4c154c26c45e57224e3a2bc5cf
willslima <williansantos38@gmail.com>
2024-02-05 17:41:25 -0300

    primeiro módulo golang finalizado

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // if comando == 1 {
@@ -17,20 +24,23 @@ import (
 
 func main() {
 	exibeIntroducao()
-	exibirMenu()
-	comando := leComando()
+	for {
+		exibirMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("A opção escolhida não existe")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+			imprimeLog()
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("A opção escolhida não existe")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -53,4 +63,79 @@ func leComando() int {
 	fmt.Scanf("%d", &comandoLido)
 	fmt.Println("A opção escolhida foi", comandoLido)
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := leSitesDoArquivo()
+	for i := 0; i < 5; i++ {
+		time.Sleep(5 * time.Second)
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+	}
+
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+	}
+	if resp.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso!")
+		registraLog(site, true)
+	} else {
+		fmt.Println("Site", site, "está com problema. Status Code:", resp.StatusCode)
+		registraLog(site, false)
+	}
+	fmt.Println("")
+}
+
+func leSitesDoArquivo() []string {
+	var sites []string
+	arquivo, err := os.Open("sites.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+	}
+	// fmt.Println(string(arquivo))
+	leitor := bufio.NewReader(arquivo)
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		// fmt.Println(linha)
+
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	fmt.Println(sites)
+	arquivo.Close()
+	return sites
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro", err)
+	}
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	fmt.Println(arquivo)
+	arquivo.Close()
+}
+
+func imprimeLog() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
